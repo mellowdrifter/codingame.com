4d5477b845bf42b4eb03f626ed8b8b8ee47fb8b9
mellowdrifter <mellow.drifter@gmail.com>
2015-07-08 11:08:12 +0100

    Create training.py

---
+++ training/training.py
@@ -0,0 +1,19 @@
+import sys, math
+
+# CodinGame planet is being attacked by slimy insectoid aliens.
+
+
+# game loop
+while 1:
+    enemy1 = raw_input() # name of enemy 1
+    dist1 = int(raw_input()) # distance to enemy 1
+    enemy2 = raw_input() # name of enemy 2
+    dist2 = int(raw_input()) # distance to enemy 2
+    
+    # Write an action using print
+    # To debug: print >> sys.stderr, "Debug messages..."
+    
+    if dist1 < dist2:
+        print enemy1
+    else:
+        print enemy2
