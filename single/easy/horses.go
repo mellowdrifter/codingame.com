f57f9f8ec8a821224304c393ca6b6f5606f66af1
mellowdrifter <mellow.drifter@gmail.com>
2015-07-08 15:14:53 +0100

    Create horses.go

---
+++ single/easy/horses.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+import "math"
+import "sort"
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+func main() {
+    var N int
+    fmt.Scan(&N)
+    
+    // Create slice of INTs
+    list := make([]int, N)
+    
+    // Smallest difference
+    difference := 100000
+    
+    
+    for i := 0; i < N; i++ {
+        //var Pi int
+        fmt.Scan(&list[i])
+    }
+    
+    // Initial sort of list
+    sort.Sort(sort.IntSlice(list))
+    
+    // Go through sorted list and find smallest difference
+    for i := 1; i < len(list); i++ {
+        currentDiff := int(math.Abs(float64(list[i] - list[i - 1])))
+        if (currentDiff < difference) {
+                difference = currentDiff
+            }
+    }
+    fmt.Println(difference)
+}
